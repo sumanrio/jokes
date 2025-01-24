c5281e532e447404aee543ac231ab2d213299b1a
sumanrio <119526968+sumanrio@users.noreply.github.com>
2025-01-24 10:50:12 +0530

    index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const btnEl = document.getElementById("btn");
 const jokeEl = document.getElementById("joke");
 
-const apiKey = "4kqGcJx8uDXo3XIskcbzokAz7rN8nWJs3PL9Mcll";
+const apiKey = "https://icanhazdadjoke.com/api";
 
 const options = {
   method: "GET",
